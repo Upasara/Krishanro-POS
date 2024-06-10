937c8d92e08d6f81eeefbfad6f1f279602aada30
arshvinw5 <tarshvin@gmail.com>
2024-06-11 00:46:07 +0530

    Sign up form with validation schema

--- components/auth/registrationForm.tsx
+++ components/auth/registrationForm.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+'use client';
 import CardWrapper from './cardWrapper';
+import { useForm } from 'react-hook-form';
+import { RegistrationSchema } from '@/schemas';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem } from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { MdOutlineAppRegistration } from 'react-icons/md';
+
+type RegistrationSchemaFields = z.infer<typeof RegistrationSchema>;
 
 export const RegistrationForm = () => {
+	const form = useForm<RegistrationSchemaFields>({
+		resolver: zodResolver(RegistrationSchema),
+		defaultValues: {
+			firstName: '',
+			lastName: '',
+			email: '',
+			password: '',
+		},
+	});
 	return (
 		<div>
 			<CardWrapper
@@ -10,7 +29,96 @@ export const RegistrationForm = () => {
 				backButtonLabel='I already have an account? Login here.'
 				backButtonHref='/login'
 				showSocial>
-				Hello
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(() => {})} className='space-y-6'>
+						<div className='space-y-4'>
+							<FormField
+								control={form.control}
+								name='firstName'
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												{...field}
+												placeholder='First Name'
+												type='text'
+												className='bg-transparent focus-visible:ring-offset-0 focus-visible:ring-0'
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='lastName'
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												{...field}
+												placeholder='Last Name'
+												type='text'
+												className='bg-transparent focus-visible:ring-offset-0 focus-visible:ring-0'
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='email'
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												{...field}
+												placeholder='Email'
+												type='email'
+												className='bg-transparent focus-visible:ring-offset-0 focus-visible:ring-0'
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='password'
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												{...field}
+												placeholder='Password'
+												type='password'
+												className='bg-transparent focus-visible:ring-offset-0 focus-visible:ring-0'
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name='confirmPassword'
+								render={({ field }) => (
+									<FormItem>
+										<FormControl>
+											<Input
+												{...field}
+												placeholder='Re-Enter your password'
+												type='password'
+												className='bg-transparent focus-visible:ring-offset-0 focus-visible:ring-0'
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</div>
+						<Button className='w-full' typeof='submit'>
+							<MdOutlineAppRegistration className='mr-2 h-5 w-5' />
+							Sign up
+						</Button>
+					</form>
+				</Form>
 			</CardWrapper>
 		</div>
 	);
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,26 +1,38 @@
 import * as z from 'zod';
 
 export const LoginSchema = z.object({
-	email: z.string().email({
-		message: 'Email is required',
+	email: z.string().min(1, 'Email is required').email({
+		message: 'Invalid email',
 	}),
 	password: z.string().min(1, {
 		message: 'Password is required',
 	}),
 });
 
-export const RegistrationSchema = z.object({
-	firstName: z.string().min(1, {
-		message: 'First Name is required',
-	}),
-	lastName: z.string().min(1, {
-		message: 'Last Name is required',
-	}),
-	email: z.string().email({
-		message: 'Email is required',
-	}),
-	password: z.string().min(6, {
-		message:
-			'Minimum six characters, at least one uppercase letter, one lowercase letter and one number are required',
-	}),
-});
+export const RegistrationSchema = z
+	.object({
+		firstName: z
+			.string()
+			.min(1, {
+				message: 'First Name is required',
+			})
+			.max(100),
+		lastName: z
+			.string()
+			.min(1, {
+				message: 'Last Name is required',
+			})
+			.max(100),
+		email: z.string().min(1, 'Email is required').email({
+			message: 'Invalid Email',
+		}),
+		password: z.string().min(1, 'Password is required').min(6, {
+			message:
+				'Minimum six characters, at least one uppercase letter, one lowercase letter and one number are required',
+		}),
+		confirmPassword: z.string().min(1, 'Password confirmation is required'),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		path: ['confirmPassword'],
+		message: 'Password do not match',
+	});
